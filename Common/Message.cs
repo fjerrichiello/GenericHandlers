492a01a9b18340b8555b3168fbf7f5826f405b7d
Frank Errichiello <fjerrichiello@gmail.com>
2024-10-31 21:16:24 -0500

    Add new files

---
+++ Common/Message.cs
@@ -0,0 +1,3 @@
+namespace Common;
+
+public abstract record Message;
---
+++ Common/MessageContainer.cs
@@ -0,0 +1,7 @@
+namespace Common;
+
+public record MessageContainer<TMessage, TMessageMetadata>(
+    TMessage Message,
+    TMessageMetadata Metadata)
+    where TMessage : Message
+    where TMessageMetadata : MessageMetadata;
